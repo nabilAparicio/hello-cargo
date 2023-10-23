95df00434ab09491f5a36a7d6046dd9528e06839
Orlando Aparicio <orlando.nabil@gmail.com>
2023-10-23 06:24:48 -0500

    manejando funciones y objetos en rust

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+
+fn main(){
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool, mileage: u32) -> Car {
+    let car: Car = {
+        color: color,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: mileage
+    };
+    return car
+};
+
+
+
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false, 2000);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true, 4000);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false, 50000);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
+
+
+}
